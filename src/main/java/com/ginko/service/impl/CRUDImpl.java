4f3a07b30dc82dd61e72620a658eb83501fe3570
ever hidalgo <ever.hidalgo22@gmail.com>
2026-05-24 13:57:56 -0500

    Se agregan dos interfaces genericas y una clase abstracta generica para realizar los CRUDS de cada entidad

---
+++ src/main/java/com/ginko/service/impl/CRUDImpl.java
@@ -0,0 +1,40 @@
+package com.ginko.service.impl;
+
+import com.ginko.exception.ModelNotFoundException;
+import com.ginko.repo.IGenericRepo;
+import com.ginko.service.ICRUD;
+
+import java.util.List;
+
+public abstract class CRUDImpl<T, ID> implements ICRUD<T, ID> {
+
+    protected abstract IGenericRepo<T, ID> getRepo();
+
+    @Override
+    public T save(T t) {
+        return getRepo().save(t);
+    }
+
+    @Override
+    public T update(ID id, T t) {
+        getRepo().findById(id).orElseThrow( ()-> new ModelNotFoundException("ID NOT FOUND: " + id));
+        return getRepo().save(t);
+    }
+
+    @Override
+    public List<T> findAll() {
+        return getRepo().findAll();
+    }
+
+    @Override
+    public T findById(ID id) {
+        getRepo().findById(id).orElseThrow( ()-> new ModelNotFoundException("ID NOT FOUND: " + id));
+        return getRepo().findById(id).orElse(null);
+    }
+
+    @Override
+    public void delete(ID id) {
+        getRepo().findById(id).orElseThrow( ()-> new ModelNotFoundException("ID NOT FOUND: " + id));
+        getRepo().deleteById(id);
+    }
+}
